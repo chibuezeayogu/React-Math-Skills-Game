99f7af23a9b044c981d39197db6b3ebcf57904d4
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2018-09-28 17:49:11 +0100

    ch(deploy): deploy base app on heroku (#14)

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use('/', express.static('dist'));
+app.use('*', express.static('dist'));
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`); // eslint-disable-line
+});
